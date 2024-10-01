c6e2e5b9c02871d3bd63316507571701ab45ef99
Antonyj26 <antonyjosu26@gmail.com>
2024-09-30 21:29:04 -0300

    feat: pagina sobre organização

---
+++ src/components/About/About.tsx
@@ -0,0 +1,33 @@
+import './About-styles.css'
+
+export function About() {
+  return (
+    <div className="About-container">
+      <h1>Sobre</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+        pulvinar finibus ultricies. Phasellus in rutrum purus, scelerisque
+        hendrerit massa. Curabitur eu eros pharetra, semper massa id, ornare
+        neque. Sed euismod sem elementum auctor mollis. Aliquam tempor ligula
+        eget ante volutpat vulputate. Phasellus fermentum facilisis est, non
+        aliquam orci euismod vitae. Phasellus pulvinar, ex non egestas commodo,
+        neque tellus rutrum est, id fermentum lectus ligula sit amet ante. Nam
+        porttitor in metus eu consectetur. Pellentesque at convallis dui. Nunc
+        maximus libero convallis tempus luctus.
+      </p>
+      <p>
+        Sed scelerisque ultrices eros, at posuere enim tristique nec. Aenean
+        eleifend ultrices ipsum, sed volutpat sem. Mauris iaculis massa est,
+        vitae lobortis velit molestie quis. Nam at vulputate augue. Curabitur
+        gravida neque eu pulvinar convallis. Donec eu ex vitae nisl faucibus
+        ullamcorper sit amet ac arcu. Nullam semper condimentum nulla, sed
+        vehicula felis. Vivamus enim enim, egestas eu erat vel, porttitor
+        fringilla mauris. Maecenas hendrerit massa quam, volutpat egestas ante
+        luctus rutrum. Cras diam arcu, tristique id scelerisque at, mattis sed
+        lorem. Fusce nibh ante, viverra ac sollicitudin quis, faucibus vulputate
+        orci.
+      </p>
+    </div>
+  )
+}
