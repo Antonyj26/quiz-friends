56e03c7a369737e1a5b3e967d4f33576f0d600da
Ricardo <ricardo.machado.nwi@gmail.com>
2024-09-30 22:58:44 -0300

    feat: header funcional

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,12 @@
 import { Footer } from '../Footer'
 import type { LayoutProps } from './Layout.types'
 import './Layout-styles.css'
+import { Header } from '../Header'
 
 export function Layout({ children }: LayoutProps) {
   return (
     <div className="Layout-container">
-      <h1>Header</h1>
+      <Header />
       <main>
         <section>
           <div className="content-container">{children}</div>
