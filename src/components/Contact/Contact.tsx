a2c7b126b11b29b0e4cb1963c0481aeb3366c686
Antonyj26 <antonyjosu26@gmail.com>
2024-10-07 21:22:12 -0300

    feat: Criado a estilização da tela de contato

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -3,7 +3,22 @@ import './Contact-styles.css'
 export function Contact() {
   return (
     <div className="Contact-container">
-      <h1>Contact</h1>
+      <h1>Contato</h1>
+      <form>
+        <fieldset>
+          <label className="subject-wrapper">
+            Assunto
+            <input
+              type="text"
+              placeholder="Digite aqui o assunto da sua mensagem"
+            />
+          </label>
+          <label className="text-wrapper">
+            Texto
+            <textarea placeholder="Digite aqui sua mensagem" />
+          </label>
+        </fieldset>
+      </form>
     </div>
   )
 }
